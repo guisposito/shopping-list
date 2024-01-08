8ea94f35cfd7d1b1e8354cfc40d44d6d76ca4cb7
Guisposito <guicarlosjoinhas@gmail.com>
2024-01-08 20:12:47 -0300

    Edit items

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 
 function displayItems() {
@@ -21,6 +23,15 @@ function onAddItemSubmit(e) {
     if (newItem.value === '') {
         alert('Please add an item');
         return;
+    }
+
+    //Check for edit mode
+    if(isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
     }
 
     addItemToDOM(newItem);
@@ -86,7 +97,22 @@ function createIcon(classes) {
 function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
         removeItem(e.target.parentElement.parentElement);
+    } else {
+        setItemToEdit(e.target);
     }
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+
+    itemList.querySelectorAll('li').forEach( i => i.classList.remove('edit-mode'));
+
+    item.classList.add('edit-mode');
+
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.style.backgroundColor = '#228B22';
+    itemInput.value = item.textContent;
+
 }
 
 function removeItem(item) {
@@ -145,6 +171,8 @@ function filterItems(e) {
 
 
 function checkUI() {
+    itemInput.value = '';
+
     const items = itemList.querySelectorAll('li');
     if (items.length === 0) {
         clearBtn.style.display = 'none';
@@ -153,6 +181,11 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
+
+    isEditMode = false;
 }
 
 // Initialize App
